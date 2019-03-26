31dff83adc5cdb69b56b5eeab352b373a194f8bc
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-03-26 10:10:29 -0300

    Add files via upload

---
+++ Exercicio1.php
@@ -0,0 +1,15 @@
+<?php
+
+Print "\nColoque o primeiro valor:\n";
+$numero1=fgets(STDIN);
+
+Print "\nColoque o segundo valor:\n";
+$numero2=fgets(STDIN);
+
+
+if ($numero1 > $numero2){
+    print"\nO numero maior é $numero1\n";
+}
+elseif ($numero2 > $numero1){
+    print"\nO numero maior é $numero2\n";
+}
