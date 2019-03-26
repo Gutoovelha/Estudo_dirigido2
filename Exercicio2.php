1540b264106e7838cc3ac0f9ff8831ab2d576590
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-03-26 10:42:57 -0300

    Add files via upload

---
+++ Exercicio2.php
@@ -0,0 +1,12 @@
+<?php
+
+Print "\nColoque o primeiro valor:\n";
+$numero1=(int)fgets(STDIN);
+
+if ($numero1 >= 0){
+    print"\nO numero $numero1 é positivo\n";
+}
+
+else {
+    print"\nO numero $numero1 é negativo\n";
+}
