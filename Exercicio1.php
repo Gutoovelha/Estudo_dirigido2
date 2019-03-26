5f459a731e785b8d97344a1685275bd349493dc9
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-03-26 10:30:50 -0300

    Update Exercicio1.php

--- Exercicio1.php
+++ Exercicio1.php
@@ -7,9 +7,10 @@ Print "\nColoque o segundo valor:\n";
 $numero2=fgets(STDIN);
 
 
-if ($numero2 > $numero1){
+if ($numero1 > $numero2){
     print"\nO numero maior é $numero1\n";
+}
 
-}elseif ($numero1 > $numero2){
-    print"\nO numero maior é $numero2\n";
+else {
+    print"\nO numero maior é:\n$numero2\n";
 }
