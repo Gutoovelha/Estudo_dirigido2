3a6e1498dd387d5f07ff46ff40b021bb1f342d7b
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-04-02 10:11:05 -0300

    Update Exercicio3.php

--- Exercicio3.php
+++ Exercicio3.php
@@ -1,11 +1,10 @@
 <?php
 Print "Digite M para masculino ou digite F para feminino\n";
 $sexo=fgetc(STDIN);
-
 if ($sexo=="M"){
     Print "Masculino\n";
 }
-
 elseif ($sexo=="F"){
     Print "Feminino\n";
 }
+else  {print "Sexo inválido\n";}
