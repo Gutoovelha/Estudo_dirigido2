5dcf854d33560f46a0ff50f9682b37a4fced94fd
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-04-02 09:19:21 -0300

    Update Exercício18.php

--- Exercício18.php
+++ Exercício18.php
@@ -1,33 +1,14 @@
 <?php
+$numeropar=0;
+$numeroimpar=0;
+$contagemvezes=1;
+do {print "Digite um número: ";
+    $num = (float) fgets(STDIN);
     
-    print "Digite o primeiro número: \n"; 
-    $numero1 = (int) fgets (STDIN);
-
-    print "Digite o segundo número: \n"; 
-    $numero2 = (int) fgets (STDIN);
-
-    print "Digite o terceiro número: \n"; 
-    $numero3 = (int) fgets (STDIN);
-
-    print "Digite o quarto número: \n"; 
-    $numero4 = (int) fgets (STDIN);
-
-    print "Digite o quinto número: \n"; 
-    $numero5 = (int) fgets (STDIN);
-
-    print "Digite o sexto número: \n"; 
-    $numero6 = (int) fgets (STDIN);
-
-    print "Digite o sétimo número: \n"; 
-    $numero7 = (int) fgets (STDIN);
-
-    print "Digite o oitavo número: \n"; 
-    $numero8 = (int) fgets (STDIN);
-
-    print "Digite o nono número: \n"; 
-    $numero9 = (int) fgets (STDIN);
-
-    print "Digite o décimo número: \n"; 
-    $numero10 = (int) fgets (STDIN);
-
+    if ($num%2==0)
+    {$numeropar++;}
+    if ($num%2!=0)
+    {$numeroimpar++;}  
+    $contagemvezes++;}  while($contagemvezes<=10);
+print "Quantidade de número(s) impar(es): $numeroimpar\nQuantidade de número(s) par(es): $numeropar";
     
