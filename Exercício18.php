7752c59a342e23e3bc47e86b0e01cfad50c1bd26
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-04-09 09:58:35 -0300

    Update Exercício18.php

--- Exercício18.php
+++ Exercício18.php
@@ -1,14 +1,18 @@
 <?php
+
 $numeropar=0;
 $numeroimpar=0;
 $contagemvezes=1;
+
 do {print "Digite um número: ";
     $num = (float) fgets(STDIN);
     
     if ($num%2==0)
     {$numeropar++;}
+    
     if ($num%2!=0)
-    {$numeroimpar++;}  
+    {$numeroimpar++;} 
     $contagemvezes++;}  while($contagemvezes<=10);
+
 print "Quantidade de número(s) impar(es): $numeroimpar\nQuantidade de número(s) par(es): $numeropar";
     
