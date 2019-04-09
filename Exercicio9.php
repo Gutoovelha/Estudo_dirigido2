b2bee700a5557ce268df70c8a51937a2985f8c09
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-04-09 11:06:42 -0300

    Update Exercicio9.php

--- Exercicio9.php
+++ Exercicio9.php
@@ -2,19 +2,16 @@
 
     print"Digite o primeiro valor: \n";
     $n1 = (int) fgets (STDIN);
-
     print"Digite o segundo valor: \n";
     $n2 = (int) fgets (STDIN);
-
     print"Digite o terceiro valor: \n";
     $n3 = (int) fgets (STDIN);
-
     if ($n1 < $n2 and $n2 < $n3){
-        print "A ordem crescente é: $n1 , $n2 , $n3\n";
+        print "1A ordem Decrescente é: $n3 , $n2 , $n1\n";
     }
-    elseif ($n2 < $n1 and $n1 < $n3){
-        print "A ordem crescente é: $n2 , $n1 , $n3\n";
+    elseif ($n2 < $n1 and $n2 < $n3){
+        print "2A ordem Decrescente é: $n3 , $n1 , $n2\n";
     }
     elseif ($n3 < $n2 and $n2 < $n1) {
-        print "A ordem crescente é: $n3 , $n2 , $n1\n";
+        print "3A ordem Decrescente é: $n1 , $n2 , $n3\n";
     }
