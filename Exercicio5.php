6964c8cd525a52dfdb30193217de0d5ef227fd21
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-04-09 10:42:29 -0300

    Update Exercicio5.php

--- Exercicio5.php
+++ Exercicio5.php
@@ -1,23 +1,23 @@
 <?php
-
-Print "\nDigite a sua primeira nota:\n";
-$P_nota=(float)fgets(STDIN);
+Print "Digite a sua primeira nota:\n";
+$Primeira_n=(float)fgets(STDIN);
 
 Print "\nDigite a sua segunda nota:\n";
-$S_nota=(float)fgets(STDIN);
+$Segunda_n=(float)fgets(STDIN);
 
-$media=($S_nota + $P_nota)/2;
-
-
+$media=($Segunda_n + $Primeira_n)/2;
 if ($media >= 7){
-    Print "\nSua nota foi $media, aluno aprovado";
+    Print "\nSua nota foi $media, aluno aprovado\n";
 }
+
 else {
-    Print "\nSua nota foi $media, aluno reprovado";
+    Print "\nSua nota foi $media, aluno reprovado\n";
 }
-if ($media = 10){
+
+if ($media == 10){
     Print "\nO aluno tirou $media na media, por isso, foi aprovado com distinçao\n";
 }
+
 else{
     Print "\nO aluno tirou $media na media, por isso, não foi aprovado com distinção\n";
 }
